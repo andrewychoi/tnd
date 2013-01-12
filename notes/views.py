e379e64a77f4d015311d99867a37e9483c5920a8
Andrew Choi <andrew@choi.com>
2013-01-12 14:40:22 -0500

    Add initial data to notes and suggestions apps, add basic functionality for notes app

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,12 @@
-# Create your views here.
+from django.shortcuts import render, redirect
 
-from django.shortcuts import redirect
+from notes.models import Note
+
+def my_notes(request):
+    td = {}
+    td["notes"] = Note.objects.all()
+    
+    return render(request, "my_notes.html", td)
 
 def get_out(request):
     return redirect("main.views.home")
